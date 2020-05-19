eb8225d76b82d77420f5a2a5c78f6429a37e2341
kdwear <65573279+kdwear@users.noreply.github.com>
2020-05-18 22:05:49 -0300

    Add files via upload

---
+++ Capstone project.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# - this notebook will be used for the final Coursera capstone project for IBM Data Science Certification.
+# 
+
+# In[2]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[3]:
+
+
+print ('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
